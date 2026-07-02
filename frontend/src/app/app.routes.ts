1f598062b8e61a10a0518c022338558f6f7c3898
gusteugenio <gustavoeugenio297@gmail.com>
2026-07-02 19:53:34 -0300

    feat: configura rotas de apolices e clientes com lazy loading

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'apolices', pathMatch: 'full' },
+  {
+    path: 'apolices',
+    loadComponent: () =>
+      import('./features/apolices/apolice-listagem/apolice-listagem.component').then(m => m.ApoliceListagemComponent)
+  },
+  {
+    path: 'clientes',
+    loadComponent: () =>
+      import('./features/clientes/cliente-listagem/cliente-listagem.component').then(m => m.ClienteListagemComponent)
+  }
+];
