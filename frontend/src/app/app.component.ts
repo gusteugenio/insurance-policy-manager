0ed67015bac012d444c80672f5d577fb82c5ebef
gusteugenio <gustavoeugenio297@gmail.com>
2026-07-02 19:53:34 -0300

    feat: integra toolbar e router-outlet no AppComponent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppToolbarComponent } from './shared/layout/app-toolbar/app-toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'frontend';
-}
+export class AppComponent {}
