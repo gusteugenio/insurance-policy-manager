7a0289c2e4942932d99264344ba2ed15484d1b6f
gusteugenio <gustavoeugenio297@gmail.com>
2026-07-02 20:26:10 -0300

    feat: adiciona rotas de novo, editar e detalhe de apólice
    - /apolices/novo, /apolices/:id/editar e /apolices/:id, fechando as rotas pendentes do CRUD de apólices

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,6 +8,21 @@ export const routes: Routes = [
       import('./features/apolices/apolice-listagem/apolice-listagem.component').then(m => m.ApoliceListagemComponent)
   },
   {
+    path: 'apolices/novo',
+    loadComponent: () =>
+      import('./features/apolices/apolice-form/apolice-form.component').then(m => m.ApoliceFormComponent)
+  },
+  {
+    path: 'apolices/:id/editar',
+    loadComponent: () =>
+      import('./features/apolices/apolice-form/apolice-form.component').then(m => m.ApoliceFormComponent)
+  },
+  {
+    path: 'apolices/:id',
+    loadComponent: () =>
+      import('./features/apolices/apolice-detalhe/apolice-detalhe.component').then(m => m.ApoliceDetalheComponent)
+  },
+  {
     path: 'clientes',
     loadComponent: () =>
       import('./features/clientes/cliente-listagem/cliente-listagem.component').then(m => m.ClienteListagemComponent)
