60ad24c5e3aca7f4468bf66a239a15c24489e765
gusteugenio <gustavoeugenio297@gmail.com>
2026-07-03 03:30:22 -0300

    fix: corrige barra duplicada na URL

--- frontend/src/environments/environment.render.ts
+++ frontend/src/environments/environment.render.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://insurance-policy-manager-production-5a9d.up.railway.app//api'
+  apiUrl: 'https://insurance-policy-manager-production-5a9d.up.railway.app/api'
 };
